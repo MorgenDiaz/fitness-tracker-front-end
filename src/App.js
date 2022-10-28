37f96d1b475c84dd3a2a96b2e2df61877be67baa
Morgen Diaz <morgen@webslinger.works>
2022-10-27 15:35:49 -1000

    Created home compnent.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import Header from "./Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
 import Register from "./routes/Register";
 
 function App() {
   return (
-    <div className="App flex flex-wrap justify-center">
-      <Header />
-
-      <div className="content-area flex grow justify-center lg:max-w-4xl bg-blue-400">
-        <Register />
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route path="/register" element={<Register />} />
+      </Route>
+    </Routes>
   );
 }
 
